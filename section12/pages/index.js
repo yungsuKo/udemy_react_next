53a03ba1b83839c4f6a02cd5122aba9710ae6cbd
yungsuKo <yungsu2391@gmail.com>
2024-07-13 18:12:53 +0900

    nextjs getserverside props

--- section12/pages/index.js
+++ section12/pages/index.js
@@ -1,7 +1,9 @@
+import { fetchCountries, fetchCoutry } from '@/api';
+import Button from '@/components/test/Button';
 import Link from 'next/link';
 import { useRouter } from 'next/router';
 
-export default function Home() {
+export default function Home({ countries }) {
   const code = 'KOR';
   const router = useRouter();
   const onClickButton = () => {
@@ -9,16 +11,18 @@ export default function Home() {
   };
   return (
     <div>
-      Home page
-      <div>
-        <button onClick={onClickButton}>search page 로 이동</button>
-      </div>
-      <div>
-        <Link href={'/search'}>Search</Link>
-      </div>
-      <div>
-        <Link href={`/country/${code}`}>{code} 페이지로 이동</Link>
-      </div>
+      {/* <Button text={'클릭'} bgColor={'black'} textColor={'white'} /> */}
+      {countries.map((country) => (
+        <div key={country.code}>{country.commonName}</div>
+      ))}
     </div>
   );
 }
+
+export const getServerSideProps = async () => {
+  // api에서 불러와서 가져오도록 수정하기
+  const countries = await fetchCountries();
+  return {
+    props: { countries },
+  };
+};
--- section12/pages/search/index.js
+++ section12/pages/search/index.js
@@ -1,7 +1,27 @@
+import { fetchCountries, fetchSearchResults } from '@/api';
 import SubLayout from '@/components/SubLayout';
 
-export default function Search() {
-  return <div>Seach page</div>;
+export default function Search({ countries }) {
+  return (
+    <div>
+      Seach page
+      {countries.map((country) => {
+        return <div key={country.code}>{country.commonName}</div>;
+      })}
+    </div>
+  );
 }
 
 Search.Layout = SubLayout;
+
+export const getServerSideProps = async (context) => {
+  // 검색어를 불러와야함
+  // 1. 검색결과로 API 호출
+  // 2. props에 검색결과를 넘김
+
+  const countries = await fetchSearchResults(context.query.q);
+  console.log(countries);
+  return {
+    props: { countries },
+  };
+};
