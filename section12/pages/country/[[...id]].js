5d76d6967161cebf2ffb3a0c7b04a446658ab80c
KO YONGSU <koyongsu@KOui-MacBookPro.local>
2024-07-27 18:14:07 +0900

    ssr : 서버사이드 렌더링, getServerSideProps

--- section12/pages/country/[[...id]].js
+++ section12/pages/country/[[...id]].js
@@ -1,10 +1,19 @@
+import { fetchCoutry } from '@/api';
 import SubLayout from '@/components/SubLayout';
 import { useRouter } from 'next/router';
 
-export default function Country() {
+export default function Country({ country }) {
   const router = useRouter();
-  const { id } = router.query;
-  return <div>Country page : {id}</div>;
+  return <div>Country page : {country.commonName}</div>;
 }
 
 Country.Layout = SubLayout;
+
+export const getServerSideProps = async (context) => {
+  const country = await fetchCoutry(context.params.id[0]);
+  return {
+    props: {
+      country,
+    },
+  };
+};
--- section12/pages/index.js
+++ section12/pages/index.js
@@ -20,6 +20,7 @@ export default function Home({ countries }) {
 }
 
 export const getServerSideProps = async () => {
+  // SSR을 위해 서버측에서 페이지 컴포넌트에게 전달할 데이터를 설정하는 함수
   // api에서 불러와서 가져오도록 수정하기
   const countries = await fetchCountries();
   return {
